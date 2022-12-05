ddca5fc243418f79bcd26c41f9a886a03da7ddb4
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-12-05 10:41:28 +0900

    adjust site header

--- space-tourism-app/src/components/Hero.jsx
+++ space-tourism-app/src/components/Hero.jsx
@@ -6,14 +6,14 @@ const Hero = () => (
     <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
       <div className="flex flex-row justify-between items-center w-full">
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:heading-[100px] leading-[75px]">
-          <span className="text-gradient text-[124px]">BEYOND</span> {" "}
+          <span className="text-gradient">BEYOND</span> {" "}
           <br/> The Universe At Your Fingertips
         </h1>
       </div>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of travel agents knows the universe like their own back pocket. Let us help you plan the adventure of a lifetime!</p>
     </div>
     <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-      <img src={earth} alt="earth" className="sm:w-[86%] w-[100%] h-[80%] relative z-[5] rounded-lg bg-blend-lighten" />
+      <img src={earth} alt="earth" className="sm:w-[80%] w-[100%] h-[100%] relative z-[5] rounded-lg bg-blend-lighten" />
       <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
       <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
       <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
